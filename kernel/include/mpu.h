3199127cef691b1daf3123d2fdb10ca5c7494f26
srutisrinidhi <30827680+srutisrinidhi@users.noreply.github.com>
2024-02-12 20:18:46 -0500

    Create mpu.h

---
+++ kernel/include/mpu.h
@@ -0,0 +1,19 @@
+/** @file    mpu.h
+ *
+ *  @brief
+ *
+ *  @date
+ *
+ *  @author
+ */
+#ifndef _MPU_H_
+#define _MPU_H_
+
+#include <unistd.h>
+
+/**
+ * @brief  Returns ceiling (log_2 n).
+ */
+uint32_t mm_log2ceil_size(uint32_t n);
+
+#endif /* _MPU_H_ */
